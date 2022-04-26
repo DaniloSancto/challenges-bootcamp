9e1c85fe04ea3b4e7e71c51bf339c5a484838ab5
Danilo Santos <danilo.santos0404@hotmail.com>
2022-04-26 05:45:13 -0300

    Home, ButtonStart styling

---
+++ challenge-two/src/components/ButtonStart/index.tsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+const ButtonStart = () => {
+  return (
+    <div className="bg-primary btn-start-container">
+      <a href="link">
+        <h6>Começar</h6>
+      </a>
+    </div>
+  );
+};
+
+export default ButtonStart;
---
+++ challenge-two/src/pages/Home/index.tsx
@@ -0,0 +1,18 @@
+import ButtonStart from "components/ButtonStart";
+import "./styles.css";
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div>
+        <h1>Desafio Github API</h1>
+        <h2>Bootcamp Spring React - DevSuperior</h2>
+      </div>
+      <div>
+          <ButtonStart/>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
