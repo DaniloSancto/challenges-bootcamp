9e290b1cb50153ca37b8105a9196d606dd4870a9
Danilo Santos <danilo.santos0404@hotmail.com>
2022-05-10 06:09:15 -0300

    search in github API

--- challenge-two/src/pages/GithubSearch/index.tsx
+++ challenge-two/src/pages/GithubSearch/index.tsx
@@ -1,31 +1,74 @@
 import "./styles.css";
 import ButtonFind from "components/ButtonFind";
 import ResultSearch from "components/ResultSearch";
+import { useState } from "react";
+import axios from "axios";
+
+type FormData = {
+  profile: string;
+};
+
+type User = {
+  avatar_url: string;
+  url: string;
+  followers: string;
+  location: string;
+  login: string;
+};
 
 const GithubSearch = () => {
+  const [formData, setFormData] = useState<FormData>({
+    profile: "",
+  });
+
+  const [user, setUser] = useState<User>();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    axios.get(`https://api.github.com/users/${formData.profile}`)
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((e) => {
+        setUser(undefined);
+      });
+  };
+
   return (
     <>
       <div className="github-search-container">
         <h1 className="search-title">Encontre um perfil Github</h1>
-        <form action="submit">
+        <form onSubmit={handleSubmit}>
           <div>
             <input
               type="text"
-              name="user-github"
+              name="profile"
+              value={formData.profile}
               className="search-input"
               placeholder="Usuário Github"
+              onChange={handleChange}
             />
             <ButtonFind />
           </div>
         </form>
       </div>
-      <ResultSearch
-        image="https://avatars.githubusercontent.com/u/63885658?v=4"
-        profile="https://api.github.com/users/DaniloSancto"
-        followers="1"
-        place="Curitiba"
-        name="Danilo Santos"
-      />
+      {user && (
+        <ResultSearch
+          image={user?.avatar_url}
+          profile={user?.url}
+          followers={user?.followers}
+          place={user?.location}
+          name={user?.login}
+        />
+      )}
     </>
   );
 };
